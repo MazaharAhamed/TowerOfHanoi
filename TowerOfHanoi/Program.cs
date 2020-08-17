546ee9031d388b007a1d58fe75c92ad4f59d9227
MazaharAhamed <mazaharahamed77@gmail.com>
2020-08-17 19:43:44 +0530

    Add project files.

---
+++ TowerOfHanoi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TowerOfHanoi
+{
+    class Program
+    {
+        public static void Hanoi(int n, char source, char temp, char dest)
+        {
+            if(n==1)
+            {
+                Console.WriteLine("Move disk " + n + " from " + source + "-->" + dest );
+                return;
+            }
+            Hanoi(n - 1, source, dest, temp);
+            Console.WriteLine("Move disk " + n + " from " + source + "-->" + dest);
+            Hanoi(n - 1, temp, source, dest);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of disks: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Hanoi(n, 'A', 'B', 'C');
+        }
+    }
+}
